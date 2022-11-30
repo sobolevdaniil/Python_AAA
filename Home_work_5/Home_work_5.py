2f7bd85a9ebe53d77375f87d2eac8688a4dbe9b5
sobolevdaniil <112568966+sobolevdaniil@users.noreply.github.com>
2022-11-30 16:44:25 +0300

    Update Home_work_5.py

--- Home_work_5/Home_work_5.py
+++ Home_work_5/Home_work_5.py
@@ -3,21 +3,19 @@ class BaseAdvert:
     def __init__(self, dictionary):
         self.dictionary = {}
         for key, value in dictionary.items():
+            if isinstance(value, dict):
+                value = BaseAdvert(value)
             self.dictionary[key] = value
 
     def __getattr__(self, key):
         value = self.dictionary.get(key)
-        if isinstance(value, dict):
-            return BaseAdvert(value)
         return value
-# out location ?
 
 
 class ColorizeMixin:
 
     def __repr__(self):
         return f'\033[0;{self.repr_color_code};48m{self.title} | {self.price} ₽\033[0m'
-# Как убрать предупреждения ?
 
 
 class Advert(ColorizeMixin):
@@ -28,6 +26,8 @@ class Advert(ColorizeMixin):
             dictionary['price'] = 0
         elif dictionary['price'] < 0:
             raise ValueError
+        if 'title' not in dictionary:
+            raise ValueError
         self.vocabulary = BaseAdvert(dictionary)
 
     def __getattr__(self, key):
@@ -35,18 +35,16 @@ class Advert(ColorizeMixin):
 
     def __repr__(self):
         return super().__repr__()
-# В задании нужно добавлять к атрибутам подчеркивание (price_) - зачем?
-# Почему нельзя обходиться без миксина?
 
 
 lesson_str = {
-              "title": "python",
-              "price": 10,
-              "location": {
-                  "address": "город Москва, Лесная, 7",
-                  "metro_stations": "Белорусская"
-                  }
+    "class_": 0,
+    "title": 'iphone',
+    "location": {
+        "address": "город Москва, Лесная, 7",
+        "metro_stations": "Белорусская"
+    }
 }
 
 ex = Advert(lesson_str)
-print(ex.price)
+print(ex)
